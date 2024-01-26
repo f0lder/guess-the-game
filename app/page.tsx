f30273f7a9033acce1bdf57018d821f7de47f39d
f0lder <ursanbogdan01@gmail.com>
2024-01-26 18:12:54 +0200

    removed alert

--- app/page.tsx
+++ app/page.tsx
@@ -167,10 +167,6 @@ export default function Home() {
 			{randomGame && (
 
 				<div className="flex flex-col justify-center items-center">
-					<div role="alert" className="alert alert-warning m-2 w-96 sm:w-3/4 md:w-1/2 lg:w-1/2 xl:w-1/3">
-						<svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
-						<span>Warning: Slow loading due to low server resources!</span>
-					</div>
 					<div className="flex justify-center m-3 overflow-hidden">
 						{DEBUG && (
 							<div>
