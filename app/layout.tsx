1aa9457a960b8f86c927bdf182413b9f34ca4a12
f0lder <admin@f0lder.xyz>
2025-01-18 15:28:03 +0200

    added more metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,11 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
 	title: "Guess the game",
 	description: "Guess the game is made with Next.js and Tailwind CSS by f0lder",
+	robots: "index, follow",
+	authors: [{ name: "f0lder", url: "https://f0lder.xyz/" }],
+	keywords: ["next.js", "tailwindcss", "typescript", "react", "game", "guess"],
+	twitter: { creator: "@f0lderDOTxyz" },
+	metadataBase: new URL("https://guess.f0lder.xyz/"),
 };
 
 export default function RootLayout({
